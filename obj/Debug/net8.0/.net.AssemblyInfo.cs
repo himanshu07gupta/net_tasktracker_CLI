58929f62afe9d1e24090697f3833db97fde5671b
himanshu07gupta <himanshu29gupta0703@gmail.com>
2024-03-06 20:08:01 +0530

    first-commit

--- obj/Debug/net8.0/.net.AssemblyInfo.cs
+++ obj/Debug/net8.0/.net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c516c79534772e6d856f5a7993e6648618c607d5")]
 [assembly: System.Reflection.AssemblyProductAttribute(".net")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
